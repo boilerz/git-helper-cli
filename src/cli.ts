32d650a7dedfad09de499eb41f30e81accea499a
momsse <1344228+momsse@users.noreply.github.com>
2022-04-22 18:59:38 +0200

    :bug: fix depracted module.parent usage

--- src/cli.ts
+++ src/cli.ts
@@ -24,4 +24,4 @@ export default function run(args: string[]): void {
 }
 
 /* istanbul ignore if */
-if (!module.parent) run(hideBin(process.argv));
+if (require.main === module) run(hideBin(process.argv));
